56c87d3cd9abc9d1f88295f02007ec0b1c666cd4
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2023-04-26 13:40:02 -0600

    finding errors in cpu alg

--- Code/ThomasAlg.py
+++ Code/ThomasAlg.py
@@ -1,5 +1,6 @@
 import numpy as np
-    
+from decimal import Decimal, getcontext
+
 
 
 def thomas_alg_cpu(M,r):
@@ -27,12 +28,14 @@ def thomas_alg_cpu(M,r):
     for i in range(1,n):
         alpha[i] = M[i,i]-M[i-1,i]*M[i,i-1]/alpha[i-1]
         beta[i] = (r[i]-M[i-1,i]*beta[i-1])/alpha[i]
-    
+    print("normal",alpha,beta)
     #Priming x solutions
     x[n-1] = beta[n-1]
 
     #Backward subsitutions for x vector
     for i in reversed(range(n-1)):
+        print(i)
+        print(M[i-1,i],alpha[i-1],beta[i-1],x[i])
         x[i-1] = beta[i-1]-M[i-1,i]*x[i]/alpha[i-1]
 
     #returning x vector
@@ -72,7 +75,53 @@ def thomas_alg_cpu_mem(M,r):
     return x
         
 
+def thomas_alg_decimal(M,r):
+    """
+    Standard thomas algorithim implemented with dsecimal library 
+    to increase the precision of the calculation. 
 
+    there have been no checks built into this
+    """
+    #finding the nxn size of matrix
+    n = len(r)
+    getcontext().prec = 50
+
+    #allocating memory to matrices
+    alpha = []
+    beta = []
+    x = []
+
+    #alpha beta matrice priming solutions
+    alpha.append(Decimal(M[0,0]))
+    beta.append(Decimal(r[0])/Decimal(M[0,0]))
+    #Forward subsitution to solve for alpha and beta
+    for i in range(1,n):
+        alpha.append(Decimal(M[i,i])-Decimal(M[i-1,i])*Decimal(M[i,i-1])/alpha[i-1])
+        beta.append((Decimal(r[i])-Decimal(M[i-1,i])*beta[i-1])/alpha[i])
+    print("Decimal",alpha,beta)
+
+    alpha2 = []
+    beta2 = []
+    for i in range(n):
+        alpha2.append(alpha[n-1-i])
+        beta2.append(beta[n-1-i])
+
+    print(alpha2,beta2)
+
+    #Priming x solutions
+    x.append(beta[n-1])
+
+    #Backward subsitutions for x vector
+    for i in range(1,n):
+        print(M[n-i-2,n-i-1],alpha2[i-1],beta2[i-1],x[i-1])
+        x.append(beta2[i]-Decimal(M[n-1-i-1,n-1-i])*x[i-1]/alpha2[i])
+    
+    x_sol = []
+    
+    for i in range(n):
+        x_sol.append(x[n-1-i])
+    #returning x vector
+    return x_sol
 
 
 
@@ -80,21 +129,24 @@ if __name__ == "__main__":
     M1 = np.array([[4,8,0,0],[8,18,2,0],[0,2,5,1.5],[0,0,1.5,1.75]])
     M2 = np.copy(M1)
 
+    print(M1)
+
     M2[3,0] = 1
     
     r = np.array([8,18,0.5,-1.75])
 
     x1 = thomas_alg_cpu(M1,r)
-    x2 = thomas_alg_cpu(M2,r)
-
-    if np.allclose(x1.dot(M1),r) and bool(~np.allclose(x2.dot(M2),r)):
-        print("Passed Fully Tridiagonal Solution")
-        
-    else:
-        print("Failed Fully Tridiagonal Test")
-
-
-    x3 = thomas_alg_cpu_mem(M1,r)
+    #x2 = thomas_alg_cpu(M2,r)
+    x3 = thomas_alg_decimal(M1,r)
     print(x1,x3)
+    #if np.allclose(x1.dot(M1),r) and bool(~np.allclose(x2.dot(M2),r)):
+    #    print("Passed Fully Tridiagonal Solution")
+    #    
+    #else:
+    #    print("Failed Fully Tridiagonal Test")
+
+
+    #x3 = thomas_alg_cpu_mem(M1,r)
+    #print(x1,x3)
 
 
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # APPM4600FinalProjectSparseSolvers
 # HYPERLINKS 
 <br>
-[Overleaf](https://www.overleaf.com/4651115148qfvnnmscsbpv)
+[Overleaf] (https://www.overleaf.com/4651115148qfvnnmscsbpv)
 
 # References
 <br>
-[Thomas Overview](https://www.sciencedirect.com/topics/engineering/thomas-algorithm#:~:text=(8.68)%20is%20an%20explicit%20iteration,%C3%97%202%20system%20of%20equations.)
+[Thomas Overview] (https://www.sciencedirect.com/topics/engineering/thomas-algorithm#:~:text=(8.68)%20is%20an%20explicit%20iteration,%C3%97%202%20system%20of%20equations.)
+[Useful Video] (https://www.youtube.com/watch?v=gPhE4KF0R-s&t=1s) 
