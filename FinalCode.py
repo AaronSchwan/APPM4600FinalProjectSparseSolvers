d4ef6af5190cfc6caace20c029d4b4ca31756c76
Aaron Schwan <43838067+AaronSchwan@users.noreply.github.com>
2023-05-02 15:11:02 -0600

    Running GPU

--- FinalCode.py
+++ FinalCode.py
@@ -258,123 +258,125 @@ if __name__ == "__main__":
     if np.allclose(vx1, vx2) and np.allclose(vx2, vx3):
         print("All methods are equal")
 
-    if True:
+    if False:
         print(
             "Begining Tests##########################################################"
         )
-        time_test_n = 5  # number of tests per n for an average
-        half_n_range = np.arange(10, pow(10, 2))
+        max_time = 400
+        time_test_n = 20  # number of tests per n for an average
+        half_n_range = np.arange(10, pow(10, 5))
 
         ################################################################################
         # Gauss Running time and memory tests
         ################################################################################
+        if False:
+            print("Begining Gauss Row Elimination Time Tests")
+            gauss_times = np.zeros([len(half_n_range), time_test_n])
 
-        print("Begining Gauss Row Elimination Time Tests")
-        gauss_times = np.zeros([len(half_n_range), time_test_n])
+            for ind_n, half_n in enumerate(half_n_range):
+                for i in range(time_test_n):
+                    start = time.time()
+                    M, r, x = generate_stiffness_matrix(half_n)
+                    vx1 = gauss_elimination(M, r)
+                    end = time.time()
+                    gauss_times[ind_n, i] = start - end
+                    
+                sys.stdout.write("\r" + str(ind_n) + " of " + str(len(half_n_range) - 1))
 
-        for ind_n, half_n in enumerate(half_n_range):
-            for i in range(time_test_n):
-                start = time.time()
-                M, r, x = generate_stiffness_matrix(half_n)
-                vx1 = gauss_elimination(M, r)
-                end = time.time()
-                gauss_times[ind_n, i] = start - end
+            np.savetxt("GaussTimes.csv", gauss_times, delimiter=",")
+            del gauss_times
 
-            sys.stdout.write("\r" + str(ind_n) + " of " + str(len(half_n_range) - 1))
+            print("\nBegining Gauss Row Elimination Memory Tests")
+            gauss_memory = np.zeros([len(half_n_range), time_test_n])
 
-        np.savetxt("GaussTimes.csv", gauss_times, delimiter=",")
-        del gauss_times
+            for ind_n, half_n in enumerate(half_n_range):
+                for i in range(time_test_n):
+                    tracemalloc.start()
+                    M, r, x = generate_stiffness_matrix(half_n)
+                    vx1 = gauss_elimination(M, r)
+                    mem = tracemalloc.get_traced_memory()
+                    gauss_memory[ind_n, i] = mem[1] - mem[0]
+                    tracemalloc.stop()
+                    sys.stdout.write(
+                        "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
+                    )
 
-        print("\nBegining Gauss Row Elimination Memory Tests")
-        gauss_memory = np.zeros([len(half_n_range), time_test_n])
-
-        for ind_n, half_n in enumerate(half_n_range):
-            for i in range(time_test_n):
-                tracemalloc.start()
-                M, r, x = generate_stiffness_matrix(half_n)
-                vx1 = gauss_elimination(M, r)
-                mem = tracemalloc.get_traced_memory()
-                gauss_memory[ind_n, i] = mem[1] - mem[0]
-                tracemalloc.stop()
-                sys.stdout.write(
-                    "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
-                )
-
-        np.savetxt("GaussMemory.csv", gauss_memory, delimiter=",")
-        del gauss_memory
-
-        ################################################################################
-        # CPU Running time and memory tests
-        ################################################################################
-
-        print("\nBegining Thomas CPU Time Tests")
-        cpu_times = np.zeros([len(half_n_range), time_test_n])
-
-        for ind_n, half_n in enumerate(half_n_range):
-            for i in range(time_test_n):
-                start = time.time()
-                x, vx = thomas_cpu(half_n)
-                end = time.time()
-                cpu_times[ind_n, i] = start - end
-                sys.stdout.write(
-                    "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
-                )
-
-        np.savetxt("ThomasCPUTimes.csv", cpu_times, delimiter=",")
-        del cpu_times
-
-        print("\nBegining Thomas CPU Memory Tests")
-        cpu_memory = np.zeros([len(half_n_range), time_test_n])
-
-        for ind_n, half_n in enumerate(half_n_range):
-            for i in range(time_test_n):
-                tracemalloc.start()
-                x, vx = thomas_cpu(half_n)
-                mem = tracemalloc.get_traced_memory()
-                cpu_memory[ind_n, i] = mem[1] - mem[0]
-                tracemalloc.stop()
-                sys.stdout.write(
-                    "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
-                )
-
-        np.savetxt("ThomasCPUMemory.csv", cpu_memory, delimiter=",")
-        del cpu_memory
+            np.savetxt("GaussMemory.csv", gauss_memory, delimiter=",")
+            del gauss_memory
 
         ################################################################################
         # CPU Running time and memory tests
         ################################################################################
+        if False:
 
-        print("\nBegining Thomas GPU Time Tests")
-        gpu_times = np.zeros([len(half_n_range), time_test_n])
+            print("\nBegining Thomas CPU Time Tests")
+            cpu_times = np.zeros([len(half_n_range), time_test_n])
 
-        for ind_n, half_n in enumerate(half_n_range):
-            for i in range(time_test_n):
-                start = time.time()
-                x, vx = thomas_gpu(half_n)
-                end = time.time()
-                gpu_times[ind_n, i] = start - end
-                sys.stdout.write(
-                    "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
-                )
+            for ind_n, half_n in enumerate(half_n_range):
+                for i in range(time_test_n):
+                    start = time.time()
+                    x, vx = thomas_cpu(half_n)
+                    end = time.time()
+                    cpu_times[ind_n, i] = start - end
+                    sys.stdout.write(
+                        "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
+                    )
 
-        np.savetxt("ThomasGPUTimes.csv", gpu_times, delimiter=",")
-        del gpu_times
+            np.savetxt("ThomasCPUTimes.csv", cpu_times, delimiter=",")
+            del cpu_times
 
-        print("\nBegining Thomas GPU Memory Tests")
-        gpu_memory = np.zeros([len(half_n_range), time_test_n])
+            print("\nBegining Thomas CPU Memory Tests")
+            cpu_memory = np.zeros([len(half_n_range), time_test_n])
 
-        for ind_n, half_n in enumerate(half_n_range):
-            for i in range(time_test_n):
-                tracemalloc.start()
-                x, vx = thomas_gpu(half_n)
-                mem = tracemalloc.get_traced_memory()
-                gpu_memory[ind_n, i] = mem[1] - mem[0]
-                tracemalloc.stop()
-                sys.stdout.write(
-                    "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
-                )
+            for ind_n, half_n in enumerate(half_n_range):
+                for i in range(time_test_n):
+                    tracemalloc.start()
+                    x, vx = thomas_cpu(half_n)
+                    mem = tracemalloc.get_traced_memory()
+                    cpu_memory[ind_n, i] = mem[1] - mem[0]
+                    tracemalloc.stop()
+                    sys.stdout.write(
+                        "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
+                    )
 
-        np.savetxt("ThomasGPUMemory.csv", gpu_memory, delimiter=",")
-        del gpu_memory
+            np.savetxt("ThomasCPUMemory.csv", cpu_memory, delimiter=",")
+            del cpu_memory
+
+        ################################################################################
+        # GPU Running time and memory tests
+        ################################################################################
+        if True:
+            print("\nBegining Thomas GPU Time Tests")
+            gpu_times = np.zeros([len(half_n_range), time_test_n])
+
+            for ind_n, half_n in enumerate(half_n_range):
+                for i in range(time_test_n):
+                    start = time.time()
+                    x, vx = thomas_gpu(half_n)
+                    end = time.time()
+                    gpu_times[ind_n, i] = start - end
+                    sys.stdout.write(
+                        "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
+                    )
+
+            np.savetxt("ThomasGPUTimes.csv", gpu_times, delimiter=",")
+            del gpu_times
+
+            print("\nBegining Thomas GPU Memory Tests")
+            gpu_memory = np.zeros([len(half_n_range), time_test_n])
+
+            for ind_n, half_n in enumerate(half_n_range):
+                for i in range(time_test_n):
+                    tracemalloc.start()
+                    x, vx = thomas_gpu(half_n)
+                    mem = tracemalloc.get_traced_memory()
+                    gpu_memory[ind_n, i] = mem[1] - mem[0]
+                    tracemalloc.stop()
+                    sys.stdout.write(
+                        "\r" + str(ind_n) + " of " + str(len(half_n_range) - 1)
+                    )
+
+            np.savetxt("ThomasGPUMemory.csv", gpu_memory, delimiter=",")
+            del gpu_memory
 
         print("\nCompletted All Tests##############################################")
